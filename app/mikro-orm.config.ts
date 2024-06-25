ef8d3d97c0253318a9f82fb1ef5565a0a2ca899b
Othniel Agera <otagera@gmail.com>
2024-06-25 17:08:39 +0100

    DB URL

--- app/mikro-orm.config.ts
+++ app/mikro-orm.config.ts
@@ -1,10 +1,13 @@
+import dotenv from "dotenv";
 import { defineConfig } from "@mikro-orm/mongodb";
 import { MongoHighlighter } from "@mikro-orm/mongo-highlighter";
 import { Log, BaseEntity } from "./entities";
 
+dotenv.config();
 export default defineConfig({
 	entities: [Log, BaseEntity],
 	dbName: "entryboost-server-node-dev",
 	highlighter: new MongoHighlighter(),
 	debug: true,
+	clientUrl: process.env.DB_URL || "mongodb://127.0.0.1:27017"
 });
